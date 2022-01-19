3519fe0b51b81a55c16e1399071c26828fdaa03a
gnipka <gnipka29@gmail.com>
2022-01-20 02:26:14 +0300

    Задание 1. Создан класс счет в банке с приватными полями, методами для доступа к данным.

---
+++ OOP/BankAccount.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP_lesson2
+{
+    /// <summary>
+    /// Тип банковского счета (перечислимый тип)
+    /// </summary>
+    public enum TypesOfBankAccount { Debit, Credit };
+    /// <summary>
+    /// счет в банке
+    /// </summary>
+    public class BankAccount
+    {
+        /// <summary>
+        /// Номер счета
+        /// </summary>
+        private long _AccountNumber;
+        /// <summary>
+        /// Баланс
+        /// </summary>
+        private double _Balance;
+        /// <summary>
+        /// Тип банковского счета (переменная)
+        /// </summary>
+        private TypesOfBankAccount _TypeOfBankAccount;
+
+        public long AccountNumber
+        {
+            get { return _AccountNumber; }
+            set
+            {
+                if(value < 0)
+                    throw new ArgumentOutOfRangeException(nameof(value), value, "Значение номера счета не может быть меньше нуля!");
+                _AccountNumber = value;
+            }
+        }
+        public double Balance { get => _Balance; set => _Balance = value; }
+        public TypesOfBankAccount TypeOfBankAccount { get => _TypeOfBankAccount; set => _TypeOfBankAccount = value; }  
+    }
+}
--- OOP/Program.cs
+++ OOP/Program.cs
@@ -1,12 +1,23 @@
 ﻿using System;
 
-namespace OOP
+namespace OOP_lesson2
 {
     class Program
     {
+        static string GetTypeBankAccount(TypesOfBankAccount typesOfBankAccount)
+        {
+            if (typesOfBankAccount == TypesOfBankAccount.Credit) 
+                return "Кредитный";
+            return "Дебетовый";
+        }
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            BankAccount bankAccount = new BankAccount();
+            bankAccount.AccountNumber = 1;
+            bankAccount.Balance = 100000;
+            bankAccount.TypeOfBankAccount = TypesOfBankAccount.Credit;
+
+            Console.WriteLine($"Номер счета: {bankAccount.AccountNumber}    Баланс: {bankAccount.Balance}   Тип банковского счета: {GetTypeBankAccount(bankAccount.TypeOfBankAccount)}");
         }
     }
 }
